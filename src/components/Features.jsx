fda8532e6ac1ef8974edb2635946527ee5b31ed5
yashraj <43349676+yashpyraj@users.noreply.github.com>
2025-11-08 12:52:47 +0530

    Updated Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -248,20 +248,6 @@ const Features = () => {
             />
           </BentoTilt>
 
-          <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1">
-            <BentoCard
-              src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg"
-              title={
-                <>
-                  <b>team hierarchy</b>
-                </>
-              }
-              description="Explore our complete organizational structure with department leads and team members."
-              ctaText="view structure"
-              onClick={() => navigate("/hierarchy")}
-            />
-          </BentoTilt>
-
           {/* <BentoTilt className="bento-tilt_2">
           <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
             <h1 className="bento-title special-font max-w-64 text-black">
